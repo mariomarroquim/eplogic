7a083a00f77cdc93f31e9efd016c7934647caabf
Mario Marroquim <mariomarroquim@ufpi.edu.br>
2021-05-25 17:22:08 -0300

    Stable minimal interface

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.ensemble          import RandomForestClassifier
 from sklearn.model_selection   import RandomizedSearchCV
 from joblib                    import dump, load
 
-vxm_model = load('notebooks/eplogic.joblib')
+eplogic = load('notebooks/eplogic.joblib')
 
 app = Flask(__name__)
 
@@ -21,27 +21,28 @@ def index():
 
 @app.route("/predict", methods=['GET'])
 def predict():
-  eplogic = load('notebooks/eplogic.joblib')
+  try:
+    eplet_data = [[(0, 1)[request.args.get('hla_locus') == 'abc'],
+                   (0, 1)[request.args.get('hla_locus') == 'drb'],
+                   (0, 1)[request.args.get('hla_locus') == 'dq'],
+                   (0, 1)[request.args.get('hla_locus') == 'dp'],
+                   int(request.args.get('panel_nc')),
+                   int(request.args.get('panel_pc')),
+                   int(request.args.get('panel_allele_count')),
+                   int(request.args.get('panel_min_mfi')),
+                   int(request.args.get('panel_max_mfi'))]]
 
-  eplet_data = [[(0, 1)[request.args.get('hla_locus') == 'abc'],
-                  (0, 1)[request.args.get('hla_locus') == 'drb'],
-                  (0, 1)[request.args.get('hla_locus') == 'dq'],
-                  (0, 1)[request.args.get('hla_locus') == 'dp'],
-                  int(request.args.get('panel_nc')),
-                  int(request.args.get('panel_pc')),
-                  int(request.args.get('panel_allele_count')),
-                  int(request.args.get('panel_min_mfi')),
-                  int(request.args.get('panel_max_mfi'))]]
 
+    results = eplogic.predict(eplet_data)
+    probabilities = eplogic.predict_proba(eplet_data)
 
-  results = eplogic.predict(eplet_data)
-  probabilities = eplogic.predict_proba(eplet_data)
+    predictions = jsonify(label=str(results[0]), score0=str(probabilities[0][0]), score1=str(probabilities[0][1]))
 
-  predictions = jsonify(label=str(results[0]), score0=str(probabilities[0][0]), score1=str(probabilities[0][1]))
-
-  response = make_response(predictions)
-  response.mimetype = 'application/json'
-  return response
+    response = make_response(predictions)
+    response.mimetype = 'application/json'
+    return response
+  except:
+    return "Please, verify if all parameters (hla_locus, panel_allele_count, panel_nc, panel_pc, panel_min_mfi, panel_max_mfi) are present and contain integer values.", 500
 
 if __name__ == "__main__":
   #app.run(host='0.0.0.0')
--- static/js/custom.js
+++ static/js/custom.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
     var data = $("#form").serialize();
 
     $.getJSON(url, data, function(response){
-      $("#result__score0").html((Math.round(response['score0'] * 100.0) / 100).toFixed(2));
-      $("#result__score1").html((Math.round(response['score1'] * 100.0) / 100).toFixed(2));
+      $("#result__score0").html((Math.round(response['score0'] * 100.0)).toFixed(2) + "%.");
+      $("#result__score1").html((Math.round(response['score1'] * 100.0)).toFixed(2) + "%.");
 
       if(response['label'] == 0){
-        $("#result__label").html('NOT ANTIBODY-REACTIVE*');
+        $("#result__label").html('IS NOT');
         $('#result').removeClass('result--positive').addClass('result--negative');
 
       } else {
-        $("#result__label").html('ANTIBODY-REACTIVE**');
+        $("#result__label").html('IS');
         $('#result').removeClass('result--negative').addClass('result--positive');
       }
 
