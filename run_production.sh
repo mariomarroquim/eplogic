d0604a8ca950efdf3ff1bfc47a6d732437f35fab
Mario Marroquim <mariomarroquim@ufpi.edu.br>
2021-05-28 10:33:21 -0300

    Waitress logging fixed

--- run_production.sh
+++ run_production.sh
@@ -3,4 +3,4 @@ killall python3
 find -type d -exec chmod 750 {} \;
 find -type f -exec chmod 640 {} \;
 
-FLASK_ENV=production nohup python3 app.py > log.txt &
+FLASK_ENV=production nohup python3 app.py &
